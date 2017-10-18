91936536148cb2fdfabb8d9e0885963225c28920
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-18 12:30:26 -0700

    Add functionality to houseCtrl

--- server/socket/controllers/houseCtrl.js
+++ server/socket/controllers/houseCtrl.js
@@ -1,9 +1,11 @@
 const joinHouse = (io, client, houseId) => {
   console.log(`hit the join house ctrl with houseId: ${houseId}`);
+  client.join(houseId);
 };
 
 const leaveHouse = (io, client, houseId) => {
   console.log(`hit the leave house ctrl with houseId: ${houseId}`);
+  client.leave(houseId);
 };
 
 module.exports = {
