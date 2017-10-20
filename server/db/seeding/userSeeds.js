f9a3391d1453847f9f9e62b9b6e51a8fbe5ce7bd
Lillian Anderson <lilliananderson@ucla.edu>
2017-10-20 16:04:41 -0700

    changed message schema to work wtih giftedchat

---
+++ server/db/seeding/userSeeds.js
@@ -0,0 +1,29 @@
+const houseSeeds = [
+  {
+    username: 'philipno@no.com',
+    firstName: 'Philepe',
+    lastName: 'No',
+    email: 'philipno@no.com',
+    phone: 5555555555,
+    houseId: 1,
+  },
+  {
+    username: 'rollinsno@no.com',
+    firstName: 'Rollins',
+    lastName: 'Cat',
+    email: 'rollinsno@no.com',
+    phone: 5555555555,
+    houseId: 1,
+  },
+  {
+    username: 'bettysueno@no.com',
+    firstName: 'BettySue',
+    lastName: 'Kitty',
+    email: 'bettysueno@no.com',
+    phone: 5555555555,
+    houseId: 2,
+  },
+];
+
+module.exports = houseSeeds;
+
