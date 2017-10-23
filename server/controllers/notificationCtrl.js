3201b2b97c1661cca408f19f79f15aca5a8ebe9c
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-23 10:35:21 -0700

    Add route and ctrl functionality for notifications

--- server/controllers/notificationCtrl.js
+++ server/controllers/notificationCtrl.js
@@ -1,14 +1,31 @@
+const Notification = require('../db/models/HouseNotification');
+
 module.exports = {
   getNotifications: (req, res) => {
-
+    Notification.findAll({
+      where: { houseId: req.params.houseId },
+      order: [['createdAt', 'DESC']],
+    })
+      .then(notifications => res.send(notifications))
+      .catch(err => console.log(`FAILED to get notifications: ${err}`));
   },
 
   createNotification: (req, res) => {
-
+    Notification.create({
+      type: req.body.type,
+      text: req.body.text,
+      houseId: req.params.houseId,
+      userId: req.body.userId,
+    })
+      .then(notification => res.status(201).send(notification))
+      .catch(err => console.log(`FAILED to create notification: ${err}`));
   },
 
   deleteNotification: (req, res) => {
+    Notification.destroy({
+      where: { id: req.body.id },
+    })
+      .then(numDestroyed => res.send(numDestroyed))
+      .catch(err => console.log(`FAILED to delete notification: ${err}`));
   },
-
-
 };
--- server/routes/notificationRouter.js
+++ server/routes/notificationRouter.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controllers/notificationCtrl');
 
-router.get('/', controller.getNotifications);
+router.get('/:houseId', controller.getNotifications);
 
-router.post('/', controller.createNotification);
+router.post('/:houseId', controller.createNotification);
 
-router.delete('/', controller.deleteNotification);
+router.delete('/:houseId', controller.deleteNotification);
 
 module.exports = router;
