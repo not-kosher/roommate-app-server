5374edf15c3f98ebf8498700bf84e892cdcb6f12
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-25 19:13:44 -0700

    Have socket emit notification after added to db

--- server/socket/controllers/notificationCtrl.js
+++ server/socket/controllers/notificationCtrl.js
@@ -1,10 +1,11 @@
 const Notification = require('../../db/models/HouseNotification');
 
 const addNotification = (io, client, notification) => {
-  io.to(notification.houseId).emit('newNotification', notification);
-
   Notification.create(notification)
-    .then(newNotification => console.log(`Added new notification to db: ${newNotification}`))
+    .then((newNotification) => {
+      console.log(`Added new notification to db: ${newNotification}`);
+      io.to(newNotification.houseId).emit('newNotification', newNotification);
+    })
     .catch(err => console.log(`FAILED to add message to db: ${err}`));
 };
 
