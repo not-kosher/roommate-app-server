b6b4e7c9e255d123ca5159bc3ad188815eda769d
Lillian Anderson <lilliananderson@ucla.edu>
2017-10-30 16:44:48 -0700

    fills out ctrl for get post and delete for Task

--- server/controllers/taskCtrl.js
+++ server/controllers/taskCtrl.js
@@ -1,13 +1,27 @@
+const Task = require('../db/models/Task')
+
 module.exports = {
   getTasks: (req, res) => {
-
+    Task.findAll({ where: { houseId: parseInt(req.params.houseId) } })
+      .then(tasks => res.send(tasks))
+      .catch(err => res.status(500).send(err));
   },
 
   createTask: (req, res) => {
-
+    Task.findOrCreate({ where: req.body })
+      .then(task => res.send(task))
+      .catch(err => res.status(500).send(err));
   },
 
   deleteTask: (req, res) => {
+    Task.findOne({ where: { id: parseInt(req.params.taskId) } })
+    .then((task) => {
+      if (task) {
+        task.destroy();
+      }
+      res.status(200).send(task);
+    })
+    .catch(err => res.status(500).send(err));
   },
 
   editTask: (req, res) => {
